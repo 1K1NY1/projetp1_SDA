6bec744992c1db5ec1f33bf2ece023eb8ee4ad66
1K1NY1 <marwanwandege@gmail.com>
2026-03-09 15:53:16 +0100

    2eme commit de MergeSort

--- MergeSort.c
+++ MergeSort.c
@@ -6,5 +6,15 @@ static void Merge(char A[],int p , int q , int r){
     int n1, n2;
     n1 = q-p+1;
     n2 = r-q;
-    // je dois créer 2 tableaux différents à partir de A
+    int i,j;
+    int k; 
+    char L[n1], R[n2];
+    for (i = 1, i<=n1 ,i++)
+    {L[i] = A[p+i-1];}
+    for ( j = 1 i<=n2 , j++)
+    {R[j] = A[q + j];}
+    L[n1]= INT_MAX; // sincèrement deepseek m'a dit que ça correspondait à l'infini pour un int donc c'est un sentinelle.
+    R[n2]= INT_MAX;
+    
+    
 }
