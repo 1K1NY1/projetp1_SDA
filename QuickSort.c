1a82e658cfc01d09fa17f4874ccff61009d19ab5
1K1NY1 <marwanwandege@gmail.com>
2026-03-09 15:05:00 +0100

    Premier commit de QuickSort

--- QuickSort.c
+++ QuickSort.c
@@ -2,3 +2,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
+
+static int partition(char A[], unsigned p, unsigned r){
+    char x;
+    int i,j;
+    x = A[r];
+    i = p-1;
+    for (j = p; j <= r - 1; j++) {
+        if (A[j]<= x)
+            i = i+1;
+            swap(A[], i, j);
+    }
+    swap(A[], i+1, r);
+
+}return i+1;
+
+void QuickSort( char A[], int p , int r){
+   int q;
+   if (p< r){
+    q = partition(A, p, r);
+    QuickSort(A,p, q-1); 
+    QuickSort(A,q+1, r );
+   }
+
+}
