6c6cbfff98f7aa83dcbd2d2110844784ea0a01e7
1K1NY1 <marwanwandege@gmail.com>
2026-03-09 15:06:13 +0100

    Premier commit de MergeSort

---
+++ MergeSort.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+static void Merge(char A[],int p , int q , int r){
+    int n1, n2;
+    n1 = q-p+1;
+    n2 = r-q;
+    // je dois créer 2 tableaux différents à partir de A
+}
