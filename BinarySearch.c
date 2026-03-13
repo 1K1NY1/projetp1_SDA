398ac84ed6aef48f06aaa1433f99417de23dbea4
mexlien <schneidersim0208@gmail.com>
2026-03-13 10:57:42 +0100

    Amélioration de l'efficacité de binarySearchLow

--- BinarySearch.c
+++ BinarySearch.c
@@ -14,10 +14,30 @@ size_t binarySearchLow(void *array, size_t length, void *key, int (*compare)(con
         int comp = compare(array,mid,key); 
         if(comp == 0)
         {
-            //trouve celui le plus à gauche
-            size_t p = 1;
-            for(; compare(array,(mid-p),key) == 0 && mid - p > 0; p++)
-            return mid - (p+1); //une incrémentation de p de trop
+            size_t last = mid;
+            size_t ref_left = mid-1;
+            size_t ref_right = mid;
+            size_t max_left = 0; //limite gauche du tableau
+            //refining
+            while(max_left + 1 < last)
+            {
+                while(compare(array,ref_left,key) == 0 && ref_left >= max_left) //tant que la clé se répète on déplace left d'un facteur 2 vers la gauche
+                {
+                    ref_right = ref_left;
+                    if(ref_left >= (last - ref_left)/2 + 1)
+                        ref_left -= (last - ref_left)/2 + 1;
+                    else
+                        break;
+                    //fprintf(stderr,"-----------------------B1//RR:%ld RL:%ld ML:%ld\n",ref_right,ref_left,max_left);
+                }
+                //fprintf(stderr,"-----------------------OUT1//RR:%ld RL:%ld ML:%ld\n",ref_right,ref_left,max_left);
+                if(ref_left < max_left)
+                        ref_left = max_left;
+                last = ref_right;
+                max_left = ref_left;
+                ref_left = last - 1;
+            }
+            return last;
         }
         if(comp > 0)
         {
