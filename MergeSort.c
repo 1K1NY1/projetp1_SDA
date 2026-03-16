29780276ad1925e0f5eda652eb4b529e4afcec0b
mexlien <schneidersim0208@gmail.com>
2026-03-16 14:13:41 +0100

    correction de MergeSort

--- MergeSort.c
+++ MergeSort.c
@@ -15,6 +15,7 @@ static void merge(size_t *tab_indices, size_t start, size_t mid, size_t end, voi
     size_t n1 = mid - start + 1; //nb element sous-tableau 1 (contient mid donc +1)
     size_t n2 = end - mid; //nb de element sous-tableau 2
 
+
     size_t *L = malloc(n1 * sizeof(size_t));
     if (L == NULL)
     {
@@ -32,12 +33,17 @@ static void merge(size_t *tab_indices, size_t start, size_t mid, size_t end, voi
     //on sépare le tableaux des indices en 2 avec L[i] qui corespond à la partie à gauche du tableaux de départ et R[j] qui correspond aux tableaux de gauche.
     //recopiage dans les deux sous tableaux
     for (size_t i = 0; i < n1; i++){
-    L[i] = tab_indices[start + i];}
-
+    L[i] = tab_indices[start + i];
+    
+}
+    
     for (size_t j = 0; j < n2; j++){ 
-    R[j] = tab_indices[mid + 1 + j];}
+    R[j] = tab_indices[mid + 1 + j];
+    
+}
+  
 
-    size_t i = 0, j = 0, k = p;
+    size_t i = 0, j = 0, k = start;
 
     /* i<n1 et j<n2 correspond à L[n1 + 1] = ∞; R[n2 + 1] = ∞ et dans ce code, on va comparer les élemens des tableaux L et R pour les trier !!
      on compare les éléments des deux sous-tableaux (L et R) pour remplir 'tab_indices' avec le plus petit disponible à chaque étape, 
@@ -79,24 +85,31 @@ void sort(void *tableau, size_t length,
 
     //Tri des indices
     merge_sort(tab_indices,0,length-1,tableau,compare);
-    printf("Indices triés : ");
-    for (size_t i = 0; i < length; i++)
-        printf("%zu ", tab_indices[i]);
-    printf("\n");
+    
     /*On parcourt le tableau d'indices triés et on déplace physiquement les données dans 'tableau' via la fonction 'swap'. Tant qu'un index n'est pas 
    à sa place, on échange l'élément actuel avec sa cible finale jusqu'à ce que tout soit aligné, puis on libère la mémoire.*/
     for (size_t i = 0; i < length; i++){
         
-        while (tab_indices[i] != i) {
-            size_t cible = tab_indices[i];
-
-            swap(tableau, i, cible);
-
-            size_t temp = tab_indices[i];
-            tab_indices[i] = tab_indices[cible];
-            tab_indices[cible] = temp;
+        if(tab_indices[i] == i) //déjà à la bonne place
+            continue;
+        size_t cible = tab_indices[i];
+        swap(tableau, i, cible);
+        //on place à la bonne place l'élément
+        
+        //mise à jour des éléments qui change de position
+        for(size_t a = i+1; a < length; a++)
+        {
+            if(tab_indices[a] == i)
+            {
+                //printf("J'échange %zu avec %zu \n", tab_indices[i],tab_indices[a]);
+                size_t temp = tab_indices[i];
+                tab_indices[i] = tab_indices[a];
+                tab_indices[a] = temp;
+            }
         }
+        
     }
+    
     free(tab_indices);
 }
 
